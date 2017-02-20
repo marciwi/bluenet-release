9ee5d683948cb6369ab2126886a8442ffb3c9a7a
Marc Hulscher <mjhulscher@gmail.com>
2017-02-20 15:04:31 +0000

    added verification to flash_firmware

--- windows_deploy/flash_firmware.bat
+++ windows_deploy/flash_firmware.bat
@@ -1,4 +1,4 @@
-::@ECHO OFF
+@ECHO OFF
 SET nrf_folder=C:\Program Files (x86)\Nordic Semiconductor\nrf5x\bin
 SET hex_file="MERGED_FIRMWARE.hex"
 
@@ -13,7 +13,7 @@ echo "Uploading Firmware"
 "%nrf_folder%\nrfjprog.exe" --family NRF52 --eraseall --log
 if %errorlevel% neq 0 pause & exit /b %errorlevel%
 
-"%nrf_folder%\nrfjprog.exe" --family NRF52 --program %hex_file% --log
+"%nrf_folder%\nrfjprog.exe" --family NRF52 --program %hex_file% --verify --log
 if %errorlevel% neq 0 echo ERROR: An error has occurred while uploading the firmware & pause & exit /b %errorlevel%
 
 "%nrf_folder%\nrfjprog.exe" --log --family nRF52 --memwr 0x10001084 --val %board_val%
